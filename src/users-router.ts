e079e24d3e3fd9f33b5b73fdfafbdc80cc999677
Last Year <soymilk@airmail.cc>
2019-05-11 14:50:34 -0400

    Find Users By Id

--- src/users-router.ts
+++ src/users-router.ts
@@ -1,20 +1,42 @@
 import express from 'express'
 
-//import { Role, User } from './model'
+import { Role } from './model'
 import { roles, users } from './state'
 
 export const usersRouter = express.Router();
 
-// Find Users
+function validateUser(req, id: number): boolean {
 
-function validateRole(req, role): boolean {
+    return req.session.user.userId === id;
+}
+
+function validateRole(req, role: Role): boolean {
 
     return req.session.user.role.roleId === role.roleId;
 }
 
+// Find Users
 usersRouter.get('', (req, res, next) => {
 
     const valid = validateRole(req, roles.finMan);
 
     valid ? res.send(users) : res.sendStatus(401);
+});
+
+// Find Users By Id
+usersRouter.get('/:id', (req, res, next) => {
+
+    const id: number = parseInt(req.params.id);
+    const valid = validateRole(req, roles.finMan) ||
+        validateUser(req, id);
+
+    if (!valid) {
+
+        res.sendStatus(401);
+    } else {
+
+        const found = users.filter(usr => usr.userId === id);
+        found.length != 0 ? res.send(found) :
+            res.status(404).send(`User of id ${id} not found`);
+    }
 });
