84d2464a1beeca895bb632f5935ec281bbd6a772
Last Year <soymilk@airmail.cc>
2019-05-17 11:32:06 -0400

    SQL scripts to create and populate database

---
+++ src/sql/create_reimbs.sql
@@ -0,0 +1,85 @@
+set schema 'reimbrs';
+
+begin;
+--rollback;
+
+/*
+ *     roleId: number     // primary key
+ *     role: string       // not null, unique
+ */
+
+drop table roles;
+create table roles(
+role_id serial primary key,
+role_name text unique not null
+);
+
+/*
+ *  userId: number     // primary key
+ *  username: string   // not null, unique
+ *  password: string   // not null
+ *  firstName: string  // not null
+ *  lastName: string   // not null
+ *  email: string      // not null
+ *  role: Role         // not null
+ */
+
+drop table users;
+create table users(
+user_id serial primary key,
+username text unique not null,
+passwd text not null,
+first_name text not null,
+last_name text not null,
+email text not null,
+role_id integer references roles (role_id) not null 
+);
+
+/*
+ *  statusId: number // primary key
+ *  status: string   // not null, unique
+ */
+
+drop table reimbrs_status;
+create table reimbrs_status(
+status_id serial primary key,
+status_name text unique not null
+);
+
+/*
+ *  typeId: number // primary key
+ *  type: string   // not null, unique
+ */
+
+drop table reimbrs_type;
+create table reimbrs_type(
+type_id serial primary key,
+type_name text unique not null
+);
+
+/*
+ *  reimbursementId: number   // primary key
+ *  author: number            // foreign key -> User, not null
+ *  amount: number            // not null
+ *  dateSubmitted: number     // not null
+ *  dateResolved: number      // not null
+ *  description: string       // not null
+ *  resolver: number          // foreign key -> User
+ *  status: number            // foreign key -> ReimbursementStatus, not null
+ *  type: number              // foreign key -> ReimbursementType
+ */
+
+drop table reimbursements;
+create table reimbursements(
+reimbrs_id serial primary key,
+author integer references users (user_id) not null,
+amount numeric not null,
+date_submit bigint not null,
+date_resolve bigint not null,
+description text not null,
+resolver integer references users (user_id),
+status_id integer references reimbrs_status (status_id) not null,
+type_id integer references reimbrs_type (type_id)
+);
+
+commit;
---
+++ src/sql/populate_reimbs.sql
@@ -0,0 +1,43 @@
+set schema 'reimbrs';
+
+begin;
+--rollback;
+
+insert into roles values
+(default, 'admin'),
+(default, 'user'),
+(default, 'finance-manager');
+
+select * from roles;
+
+insert into users values
+(default, 'admin0', 'password', 'Super', 'User', 'admin@organization.org', 1),
+(default, 'user0', 'password', 'John', 'Smith', 'peon@organization.org', 2),
+(default, 'overseer0', 'password', 'Sadi', 'Stic', 'taskmaster@organization.org', 3);
+
+select * from users;
+
+insert into reimbrs_status values
+(default, 'Pending'),
+(default, 'Approved'),
+(default, 'Denied');
+
+select * from reimbrs_status;
+
+insert into reimbrs_type values
+(default, 'Materials'),
+(default, 'Travel'),
+(default, 'Spiritual Guidance'),
+(default, 'Personal');
+
+select * from reimbrs_type;
+
+insert into reimbursements values
+(default, 2, 666.00, 1558106326804, 1558106326805, 'Black candles', 3, 2, 1),
+(default, 2, 5.00, 1558106326805, 1558106326806, 'Toothpaste', 3, 3, 4),
+(default, 2, 30.00, 1558106326806, 1558106326807, 'Taxi to airport', 3, 3, 2),
+(default, 1, 1500.00, 1558106326807, 1558106326808, 'Beeswax', 3, 2, 1);
+
+select * from reimbursements;
+
+commit;
